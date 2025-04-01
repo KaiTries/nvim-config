9662f48d28f6417eead0799dbbed7f6e121d1ef5
Kai <112092835+KaiTries@users.noreply.github.com>
2025-04-01 19:21:07 +0200

    updates

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,62 +1,63 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    },
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      auto_install = true,
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.ccls.setup({
-        capabilities = capabilities,
-        cmd = {
-          "ccls",
-          "--offset-encoding=utf-16",
-           },
-      })
-      lspconfig.pyright.setup({
-        capabilities = capabilities,
-      })
-      lspconfig.eslint.setup({
-        capabilities = capabilities,
-        flags = { debounce_text_changes = 500 },
-        on_attach = function(client, bufnr)
-          client.server_capabilities.document_formatting = true
-          if client.server_capabilities.document_formatting then
-            local au_lsp = vim.api.nvim_create_augroup("eslint_lsp", { clear = true })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-              pattern = "*",
-              callback = function()
-                vim.lsp.buf.format(nil)
-              end,
-              group = au_lsp,
-            })
-          end
-        end,
-      })
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		},
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			auto_install = true,
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "saghen/blink.cmp" },
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
-      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-    end,
-  },
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.ccls.setup({
+				capabilities = capabilities,
+				cmd = {
+					"ccls",
+					"--offset-encoding=utf-16",
+				},
+			})
+			lspconfig.pyright.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.eslint.setup({
+				flags = { debounce_text_changes = 500 },
+				on_attach = function(client, bufnr)
+					client.server_capabilities.document_formatting = true
+					if client.server_capabilities.document_formatting then
+						local au_lsp = vim.api.nvim_create_augroup("eslint_lsp", { clear = true })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							pattern = "*",
+							callback = function()
+								vim.lsp.buf.format(nil)
+							end,
+							group = au_lsp,
+						})
+					end
+				end,
+			})
+			vim.diagnostic.config({ virtual_lines = { current_line = true } })
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
